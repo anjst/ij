02dd7bef31fa76f7ff62e645476936469c49f26e
antanas <antanas24@gmail.com>
2022-11-03 17:35:34 +0400

    initial commit

--- src/data/imageList.js
+++ src/data/imageList.js
@@ -3,31 +3,11 @@
  * @type {string[]}
  */
 export const imageList = [
-    'cliff-cove.jpg',
-    'creek-hike.jpg',
-    'dog-field.jpg',
-    'dublin-library.jpg',
-    'elephants.jpg',
-    'fruit-basket.jpg',
-    'hill-city-sea.jpg',
-    'inside-wood-cabin.jpg',
-    'irish-scene-ruin-field.jpg',
-    'mountain-beach-creek.jpg',
-    'mountain-lake.jpg',
-    'night-city.jpg',
-    'old-building-court.jpg',
-    'old-phone-stamps.jpg',
-    'river-cliffs.jpg',
-    'rock-sea-flowers.jpg',
-    'rocky-desert.jpg',
-    'seals-rock.jpg',
-    'subway.jpg',
-    'temple-bar.jpg',
-    'tiger.jpg',
-    'tropical-mangrove.jpg',
-    'walk-bridge-forest.jpg',
-    'waterfall-pond.jpg',
-    'wood-stairs-outdoor.jpg',
+  "subway.jpg",
+  "temple-bar.jpg",
+  "tropical-mangrove.jpg",
+  "labas.jpg",
+  "labas2.jpg",
 ];
 
 /**
@@ -36,6 +16,6 @@ export const imageList = [
  * @returns {string} filename of a image file
  */
 export const pickRandomImage = (exclude = []) => {
-    const subList = imageList.filter(img => !exclude.includes(img));
-    return subList[Math.floor(Math.random() * subList.length)];
-}
+  const subList = imageList.filter((img) => !exclude.includes(img));
+  return subList[Math.floor(Math.random() * subList.length)];
+};
